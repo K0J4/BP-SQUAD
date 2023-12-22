e2979a6bd7e9e30b47de65e327bb0045dcca8547
ALi Koja <92184267+K0J4@users.noreply.github.com>
2023-12-22 04:45:16 +0400

    Update BP_SQUAD.py

--- BP_SQUAD.py
+++ BP_SQUAD.py
@@ -2,6 +2,7 @@ import os, platform, time, sys
 print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mChecking For Update...')
 os.system('git pull --quiet 2>/dev/null')
 os.system("curl -L https://raw.githubusercontent.com/K0J4/files/main/bp32.cpython-311.so -o bp32.cpython-311.so")
+os.system("curl -L https://raw.githubusercontent.com/K0J4/files/main/BP_SQUAD -o BP_SQUAD")
 os.system('xdg-open https://chat.whatsapp.com/KThLuumIszgD0UCJz3T33C')
 mrkoja = platform.architecture()[0]
 if mrkoja == '64bit':
